2d6097b98fb9dd6f9042e61496dfded2b258456f
darwishdev <a.darwish.dev@gmail.com>
2025-03-07 16:46:34 +0200

    multi select input

--- docs/src/views/AppForm.vue
+++ docs/src/views/AppForm.vue
@@ -4,34 +4,43 @@ import type { UserCreateUpdateRequest } from "@buf/ahmeddarwish_devkit-api.bufbu
 import { AppForm } from "devkit-admin";
 import type { AppFormSections } from "../../../devkit-admin/dist/types/pkg/types/types";
 const { t } = useI18n()
+const getYesterdayTodayTomorrow = () => {
+  const today = new Date();
+  const yesterday = new Date(today);
+  yesterday.setDate(today.getDate() - 1);
+  const tomorrow = new Date(today);
+  tomorrow.setDate(today.getDate() + 1);
+
+  return [yesterday, today, tomorrow];
+}
 const formSections: AppFormSections<any> = {
   'user_info': {
     isTitleHidden: true,
     isTransparent: true,
     inputs: [
       {
-        $formkit: 'devkitDropdown',
+        $formkit: 'devkitMultiDropdown',
         options: 'cityListInput',
         filter: true,
         filterPlaceholder: 'search',
         prefixIcon: "tools",
         outerClass: "col-12 sm:col-6 md:col-5",
+        responseOptionsKey: "options",
         name: "cityId",
         cacheKey: 'city',
         useLazy: true,
         debounceInMilliSeconds: 3000,
         placeholder: "city",
-        createRoute: "/properties/city/create",
         label: t("city")
       },
       {
-        $formkit: 'devkitDropdown',
+        $formkit: 'devkitMultiDropdown',
         options: 'locationListInput',
         filter: true,
         filterPlaceholder: 'search',
         dependsOn: "cityId",
         cacheKey: 'location',
-        requestPropertyName: 'cityId',
+        requestPropertyName: 'cityIds',
         prefixIcon: "tools",
         outerClass: "col-12 sm:col-6 md:col-5",
         name: "locationId",
@@ -41,6 +50,16 @@ const formSections: AppFormSections<any> = {
         createRoute: "/accounts/user/create",
         label: t("locationName")
       },
+      {
+        $formkit: 'devkitDatepicker',
+        disabledDates: getYesterdayTodayTomorrow,
+        name: "date",
+        value: 20240302,
+        convertToNumber: true,
+        placeholder: "date",
+        label: t("date")
+      }
+
     ]
   }
 }
--- docs/src/views/BaseAppIcon.vue
+++ docs/src/views/BaseAppIcon.vue
@@ -1,28 +1,132 @@
 <script lang="ts" setup>
-import { useI18n } from 'vue-i18n';
+import { ref } from 'vue';
+import { usePrimeVue } from 'primevue/config';
+import FileUpload from 'primevue/fileupload';
+import { useToast } from "primevue/usetoast";
+import Button from 'primevue/button';
+import ProgressBar from 'primevue/progressbar';
+import Badge from 'primevue/badge';
+import Toast from 'primevue/toast';
+const $primevue = usePrimeVue();
+const toast = useToast();
 
+const totalSize = ref<number>(0);
+const totalSizePercent = ref<number>(0);
+const files = ref<any[]>([]);
 
-const schema = [
-  {
-    $formkit: 'text',
-    label: "name",
-    placeHolder: "hoelo",
-    prefixIcon: "email"
+const onRemoveTemplatingFile = (file: any, removeFileCallback: (index: number) => void, index: number): void => {
+  removeFileCallback(index);
+  if (file && file.size) {
+    totalSize.value -= parseInt(formatSize(file.size));
+    totalSizePercent.value = totalSize.value / 10;
   }
-]
-const submitHandler = () => null
-const { t } = useI18n()
-</script>
+};
 
+const onClearTemplatingUpload = (clear: () => void): void => {
+  clear();
+  totalSize.value = 0;
+  totalSizePercent.value = 0;
+};
+
+const onSelectedFiles = (event: any): void => {
+  if (event.files instanceof Array) {
+    files.value = event.files as any[];
+    files.value.forEach((file: any) => {
+      if (file && file.size) {
+        totalSize.value += parseInt(formatSize(file.size));
+      }
+    });
+  }
+};
+
+const uploadEvent = (callback: () => void): void => {
+  totalSizePercent.value = totalSize.value / 10;
+  callback();
+};
+
+const onTemplatedUpload = (): void => {
+  toast.add({ severity: "info", summary: "Success", detail: "File Uploaded", life: 3000 });
+};
+
+const formatSize = (bytes: number): string => {
+  const k = 1024;
+  const dm = 3;
+  const sizes = $primevue.config.locale!.fileSizeTypes;
+
+  if (bytes === 0) {
+    return `0 ${sizes[0]}`;
+  }
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  const formattedSize = parseFloat((bytes / Math.pow(k, i)).toFixed(dm));
+
+  return `${formattedSize} ${sizes[i]}`;
+};
+</script>
 <template>
-  <h2>hi : {{ t('hello') }}</h2>
-  <FormKit type="form" :actions="true" :onsubmit="submitHandler" :submit-attrs="{
-    inputClass: 'my-input-class',
-    wrapperClass: 'my-wrapper-class',
-    'data-theme': `dark`,
-    help: 'My button help text',
-    ignore: false
-  }">
-    <FormKitSchema :schema="schema" />
-  </FormKit>
+  <div class="card">
+    <Toast />
+    <FileUpload name="demo[]" url="/api/upload" @upload="onTemplatedUpload" :multiple="true" accept="image/*"
+      :maxFileSize="1000000" @select="onSelectedFiles">
+      <template #header="{ chooseCallback, uploadCallback, clearCallback, files }">
+        <div class="flex flex-wrap justify-between items-center flex-1 gap-4">
+          <div class="flex gap-2">
+            <Button @click="chooseCallback()" icon="pi pi-images" rounded outlined severity="secondary"></Button>
+            <Button @click="uploadEvent(uploadCallback)" icon="pi pi-cloud-upload" rounded outlined severity="success"
+              :disabled="!files || files.length === 0"></Button>
+            <Button @click="clearCallback()" icon="pi pi-times" rounded outlined severity="danger"
+              :disabled="!files || files.length === 0"></Button>
+          </div>
+          <ProgressBar :value="totalSizePercent" :showValue="false" class="md:w-20rem h-1 w-full md:ml-auto">
+            <span class="whitespace-nowrap">{{ totalSize }}B / 1Mb</span>
+          </ProgressBar>
+        </div>
+      </template>
+      <template #content="{ files, uploadedFiles, removeUploadedFileCallback, removeFileCallback }">
+        <div class="flex flex-col gap-8 pt-4">
+          <div v-if="files.length > 0">
+            <h5>Pending</h5>
+            <div class="flex flex-wrap gap-4">
+              <div v-for="(file, index) of files" :key="file.name + file.type + file.size"
+                class="p-8 rounded-border flex flex-col border border-surface items-center gap-4">
+                <div>
+                  <img role="presentation" :alt="file.name" :src="file.objectURL" width="100" height="50" />
+                </div>
+                <span class="font-semibold text-ellipsis max-w-60 whitespace-nowrap overflow-hidden">{{ file.name
+                  }}</span>
+                <div>{{ formatSize(file.size) }}</div>
+                <Badge value="Pending" severity="warn" />
+                <Button icon="pi pi-times" @click="onRemoveTemplatingFile(file, removeFileCallback, index)" outlined
+                  rounded severity="danger" />
+              </div>
+            </div>
+          </div>
+
+          <div v-if="uploadedFiles.length > 0">
+            <h5>Completed</h5>
+            <div class="flex flex-wrap gap-4">
+              <div v-for="(file, index) of uploadedFiles" :key="file.name + file.type + file.size"
+                class="p-8 rounded-border flex flex-col border border-surface items-center gap-4">
+                <div>
+                  <img role="presentation" :alt="file.name" :src="file.objectURL" width="100" height="50" />
+                </div>
+                <span class="font-semibold text-ellipsis max-w-60 whitespace-nowrap overflow-hidden">{{ file.name
+                  }}</span>
+                <div>{{ formatSize(file.size) }}</div>
+                <Badge value="Completed" class="mt-4" severity="success" />
+                <Button icon="pi pi-times" @click="removeUploadedFileCallback(index)" outlined rounded
+                  severity="danger" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </template>
+      <template #empty>
+        <div class="flex items-center justify-center flex-col">
+          <i class="pi pi-cloud-upload !border-2 !rounded-full !p-8 !text-4xl !text-muted-color" />
+          <p class="mt-6 mb-0">Drag and drop files to here to upload.</p>
+        </div>
+      </template>
+    </FileUpload>
+  </div>
 </template>
