cd3a0161e4aadbc97469f023ea68bc1c810524cd
darwishdev <a.darwish.dev@gmail.com>
2025-04-01 18:46:31 +0200

    work on datalist v2

--- devkit-admin/src/app/datalistv2/store/DatalistStore.ts
+++ devkit-admin/src/app/datalistv2/store/DatalistStore.ts
@@ -4,8 +4,9 @@ import { DatalistState, DatalistStore } from './types'
 import { TestAPI } from 'vitest'
 import { DatalistProps } from '../types'
 import { FormKitSchemaNode } from '@formkit/core'
-import { ObjectKeys } from 'devkit-apiclient'
-import { DatalistFilter } from '@/app/datalist/utilities/_filtersTypes'
+import { ObjectKeys, StringUnkownRecord } from 'devkit-apiclient'
+import { DatalistFilter, DatalistFiltersModel } from '@/app/datalist/utilities/_filtersTypes'
+import { RouteQueryFind } from '@/pkg/utils/QueryUtils'
 
 export const useDatalistStore = <
 	TApi extends Record<string, Function>,
@@ -16,15 +17,29 @@ export const useDatalistStore = <
 	TFormSectionsRequest extends Record<string, unknown> | undefined = undefined>
 	({ context }: DatalistProps<TApi, TReq, TRecord, TFiltersReq, TApiResponse, TFormSectionsRequest>) => defineStore(`datalist-${context.datalistKey}` as string, () => {
 		const filtersFormSchema: FormKitSchemaNode[] = []
+		const modelFiltersRef: Ref<DatalistFiltersModel> = ref({})
+		const filtersFormKey = "filters"
+		const filtersFormInitialValues = () => {
+			const formValues =
+				localStorage.getItem(filtersFormKey) ||
+				RouteQueryFind(filtersFormKey);
+			let formValuesObject: StringUnkownRecord = {}
+			if (formValues != null) {
+				formValuesObject = JSON.parse(formValues)
+			}
+			return formValuesObject
+		}
 		const generateFilters = () => {
+			const initialValues = filtersFormInitialValues()
 			console.log(context.clientFilters, context.serverFilters, context.columns)
 			const { clientFilters, serverFilters, columns } = context
 			let clientFiltersLength = 0
 			if (clientFilters) {
 				clientFiltersLength = clientFilters.length
 				clientFilters.forEach((clientFilter, index) => {
+					const inputName = clientFilter.input.name as string
+					modelFiltersRef.value[inputName] = { matchMode: clientFilter.matchMode, value: initialValues[inputName] }
 					filtersFormSchema[index] = clientFilter.input
-					console.log("client filter is", clientFilter)
 				})
 			}
 			if (serverFilters) {
@@ -38,6 +53,8 @@ export const useDatalistStore = <
 					console.log("columns is", columns[column].filters)
 					if (columns[column].filters)
 						columns[column].filters.forEach((filter: DatalistFilter<TRecord>) => {
+							const inputName = filter.input.name as string
+							modelFiltersRef.value[inputName] = { matchMode: filter.matchMode, value: initialValues[inputName] }
 							filtersFormSchema.push(filter.input)
 						})
 				})
